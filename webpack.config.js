ace23615e891c1662713afc8c21b63c44862b780
LynnGe <nzgezilin@gmail.com>
2016-03-10 15:09:43 +1300

    webpack setup   react, es6, sass/less, hot reload

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
+var config = {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'app/main.js')
+    ],
+     resolve: {
+        alias: {
+          'react': pathToReact
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+     module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader'
+            } , {
+                test: /\.css$/, // Only .css files
+                loader: 'style!css' // Run both loaders
+            }, {
+                test: /\.less$/,
+                loader: 'style!css!less'
+            }, {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            },  {
+                test: /\.(png|jpg)$/,
+                loader: 'url?limit=25000'
+            },  {
+                test: /\.woff$/,
+                loader: 'url?limit=100000'
+            }],
+          noParse: [pathToReact]
+    }
+};
+
+module.exports = config;
