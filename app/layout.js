dbebd58c82b65b95a9f4ecd64be34c00eabda74b
LynnGe <nzgezilin@gmail.com>
2016-03-11 17:28:04 +1300

    Customize Navbars 	add logo 	change style

--- app/layout.js
+++ app/layout.js
@@ -1,42 +1,56 @@
 import React from 'react';
-
+import "./layout.scss";
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { NavItem } from 'react-bootstrap';
 import { NavDropdown } from 'react-bootstrap';
 import { MenuItem } from 'react-bootstrap';
 
-import "./layout.scss";
+import { Image } from 'react-bootstrap';
+
+
+class ImageResponsiveInstance extends React.Component {
+  render() {
+    return (<Image src="images/logo.png" className='logo' responsive />);
+
+  }
+};
+
 
 
 export default class MyReactBootstrapNavbarInstance extends React.Component {
   render() {
     return (
       <div>
-      <Navbar inverse>
+      <Navbar inverse className='header_nav'>
+
       <Navbar.Header>
       <Navbar.Brand>
-      <a href="#">React-Bootstrap</a>
+      <ImageResponsiveInstance />
       </Navbar.Brand>
       <Navbar.Toggle />
       </Navbar.Header>
+
       <Navbar.Collapse>
+
       <Nav>
-      <NavItem eventKey={1} href="#">Link</NavItem>
-      <NavItem eventKey={2} href="#">Link</NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      <NavItem eventKey={1} href="#">HOME</NavItem>
+      <NavItem eventKey={2} href="#">ABOUT</NavItem>
+      <NavItem eventKey={3} href="#">PORTFOLIO</NavItem>
+      <NavItem eventKey={4} href="#">SERVICES</NavItem>
+      <NavItem eventKey={5} href="#">FAQ</NavItem>
+      <NavItem eventKey={6} href="#">CONTACT</NavItem>
+      </Nav>
+
+      <Nav pullRight>
+      <NavDropdown eventKey={2} title="Language" id="basic-nav-dropdown">
+      <MenuItem eventKey={2.1}>English</MenuItem>
+      <MenuItem eventKey={2.2}>Chinese</MenuItem>
       </NavDropdown>
       </Nav>
-      <Nav pullRight>
-      <NavItem eventKey={1} href="#">Link Right</NavItem>
-      <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
+
       </Navbar.Collapse>
+
       </Navbar>
       </div>);
   }
