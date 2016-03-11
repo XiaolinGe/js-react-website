a918544dc8bd5ea726eacc9353ff017668a56c21
LynnGe <nzgezilin@gmail.com>
2016-03-11 14:33:43 +1300

    Sass test 	install sass, css-loader, style-loader, node-sass 	test sass can work

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { ButtonGroup } from 'react-bootstrap';
+import "./layout.scss";
 
 
 export default class MyReactBootstrapButton extends React.Component {
   render() {
-    return  (<div>
+    return  (<div className='sass_test'>
+      <p>this is sass test</p>
       <ButtonGroup>
       <Button>Left</Button>
       <Button>Middle</Button>
