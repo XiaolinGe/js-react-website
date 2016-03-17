5e371d0d0713b8519b677c443263e97700c94b56
LynnGe <nzgezilin@gmail.com>
2016-03-17 17:04:49 +1300

    Portfolio extract data     Extracting data from Portfolio component in portfolio page     Just using one map not map nest map

--- app/portfolio.js
+++ app/portfolio.js
@@ -18,21 +18,19 @@ class PortfolioRow extends  React.Component {
       <div className="layout_banner">
       <h1>portfolio</h1>
       </div>
-      <Grid className="portfolio_grid">
-      {portfolio.map((e,index) => (
-      <Row className="show-grid portfolio_row" key={index} >
-       {e.map( ({href, src, alt, id},index) =>
-        (
-          <Col md={3} key={index} className="portfolio_single">
-          <a href={href}>
-          <Image src={src} responsive alt={alt} id={id} className="portfolio_image" />
-          </a>
-          </Col>
-        ))}
-
+      <Grid className="portfolio">
+      <Row className="show-grid portfolio_row">
+        {portfolio.map( ({href, src, alt, id},index) =>
+          (
+            <Col md={3} key={index} className="portfolio_single">
+            <a href={href}>
+            <Image src={src} responsive alt={alt} id={id} className="portfolio_image" />
+            </a>
+            </Col>
+            ) ) }
         </Row>
-      ) ) }
-      </Grid>
+     </Grid>
+
 </div>
     );
   }
@@ -55,17 +53,19 @@ export default class Portfolio extends React.Component {
 
 
 
-let portfolio = [
+let portfolio =
   [
     {href: "#", src: "workImages/mekongBaby.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workImages/mekongBaby.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workImages/mekongBaby.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workImages/mekongBaby.jpg", alt: "mekongBaby", id: "mekongBaby"}
-  ],
-  [
+    {href: "#", src: "workImages/coffeeLab.jpg", alt: "coffeeLab", id: "coffeeLab"},
+    {href: "#", src: "workImages/burgerfuel.jpg", alt: "burgerfuel", id: "burgerfuel"},
+    {href: "#", src: "workImages/dutchdelight.jpg", alt: "dutchdelight", id: "dutchdelight"},
+
     {href: "#", src: "workimages/mimosa.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workimages/mimosa.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workimages/mimosa.jpg", alt: "mekongBaby", id: "mekongBaby"},
-    {href: "#", src: "workimages/mimosa.jpg", alt: "mekongBaby", id: "mekongBaby"}
+    {href: "#", src: "workimages/alfornoitalian.jpg", alt: "alfornoitalian", id: "alfornoitalian"},
+    {href: "#", src: "workimages/vmaxcafe.jpg", alt: "vmaxcafe", id: "vmaxcafe"},
+    {href: "#", src: "workimages/glapogenuine.jpg", alt: "glapogenuine", id: "glapogenuine"},
+
+    {href: "#", src: "workimages/vmaxcafe.jpg", alt: "vmaxcafe", id: "vmaxcafe"},
+    {href: "#", src: "workimages/vmaxcafe.jpg", alt: "vmaxcafe", id: "vmaxcafe"},
+    {href: "#", src: "workimages/glapogenuine.jpg", alt: "glapogenuine", id: "glapogenuine"}
   ]
-]
